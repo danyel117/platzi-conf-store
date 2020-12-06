d9608fcacd72ffd6da08f4c8b969fb81f779af0b
danyel117 <dsaldarriaga@prevalentware.com>
2020-12-05 23:08:35 -0500

    Clase 21: agregar arquitectura de componentes Home, information, payment y success

---
+++ pages/checkout/information/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Information = () => <div>Information</div>;
+
+export default Information;
---
+++ pages/checkout/payment/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Payment = () => <div>Payment</div>;
+
+export default Payment;
