fa604b0c6e53a82fc107ded1d8f48e2da940b709
danyel117 <dsaldarriaga@prevalentware.com>
2020-12-05 22:26:35 -0500

    inicialización del proyecto platzi conf store

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,7 @@
-import {ThemeContext} from 'context/theme'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ThemeContext.Provider value="blue">
       <Component {...pageProps} />
-    </ThemeContext.Provider>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,17 @@
 import React from 'react';
 // import Head from 'next/head';
-import Header from '@components/Header'
-import Characters from '@components/Characters'
+
 
 export default function Home() {
   return (
     <div>
-      {/* <Head>
-        <title>My page title</title>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-      </Head> */}
-      <main>
-        <Header/>
-        <h1>hola Mundo</h1>
-        <Characters/>
-      </main>
+      <h1>Hola Mundo</h1>
+      <style jsx>{`
+        h1 {
+          font-size: 40px;
+          color: blue;
+        }
+      `}</style>
     </div>
   );
 }
