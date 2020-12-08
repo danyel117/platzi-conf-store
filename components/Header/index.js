8d199c38f0ffc448b2de9d9ba679f6af6c2c5186
danyel117 <dsaldarriaga@prevalentware.com>
2020-12-08 13:17:19 -0500

    clase 29: agregar items al carrito

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import Link from 'next/link';
+import {useAppContext} from 'context/AppContext'
 import { HeaderS, Checkout } from './styles';
 
-const Header = () => (
-  <HeaderS>
-    <Link href="/">
-      <h1>PlatziConf Merch</h1>
-    </Link>
-    <Checkout>
-      <Link href="/checkout">
-        <i className="fas fa-shopping-basket" />
+const Header = () => {
+  const {state} = useAppContext()
+  const {cart} = state
+  return(
+    <HeaderS>
+      <Link href="/">
+        <h1>PlatziConf Merch</h1>
       </Link>
-    </Checkout>
-  </HeaderS>
-);
+      <Checkout>
+        <Link href="/checkout">
+          <i className="fas fa-shopping-basket" />
+        </Link>
+        {cart.length>0 && (
+        <div>
+          {cart.length}
+        </div>
+      )}
+      </Checkout>
+    </HeaderS>
+  )
+};
 
 export default Header;
--- components/Products/index.js
+++ components/Products/index.js
@@ -1,4 +1,6 @@
-import React, { useState, useEffect } from 'react';
+/* eslint-disable react/jsx-one-expression-per-line */
+import React from 'react';
+import {useAppContext} from 'context/AppContext'
 import {
   ProductItems,
   ProductItem,
@@ -7,19 +9,28 @@ import {
   ProductImage,
 } from './styles';
 
-const Products = ({ products }) => (
-  <div>
-    <ProductItems>
-      {products.map((product) => (
-        <Product key={product.id} product={product} />
-      ))}
-    </ProductItems>
-  </div>
-);
+const Products = () => {
+  const {state,addToCart} = useAppContext()
+  const {products} = state
+
+  const handleAddToCart = (product)=>{
+    addToCart(product)
+  }
+
+  return(
+    <div>
+      <ProductItems>
+        {products.map((product) => (
+          <Product key={product.id} product={product} handleAddToCart={handleAddToCart} />
+        ))}
+      </ProductItems>
+    </div>
+  )
+};
 
 export default Products;
 
-const Product = ({ product }) => (
+const Product = ({ product, handleAddToCart }) => (
   <ProductItem>
     <ProductImage
       src={product.image}
@@ -33,7 +44,7 @@ const Product = ({ product }) => (
         <span>${product.price}</span>
       </h2>
       <p>{product.description}</p>
-      <Button>Comprar</Button>
+      <Button onClick={() => handleAddToCart(product)}>Agregar al carrito</Button>
     </ProductItemInfo>
   </ProductItem>
 );
