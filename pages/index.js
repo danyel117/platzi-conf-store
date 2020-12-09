af81a4540db23d64c9d5ab27e9010522b5f8fc71
danyel117 <dsaldarriaga@prevalentware.com>
2020-12-08 22:58:38 -0500

    agregar head en home y checkout para mejorar SEO

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Head from 'next/head'
 import Layout from '@components/Layout';
 import Products from '@components/Products';
 import ProductList from 'initialState';
@@ -6,8 +7,33 @@ import ProductList from 'initialState';
 
 export default function Home() {
   return (
-    <Layout>
-      <Products products={ProductList.products} />
-    </Layout>
+    <>
+      <Head>
+        <title>PlatziConf Store - Productos</title>
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@TU_USER" />
+        <meta name="twitter:creator" content="@TU_USER" />
+        <meta name="twitter:title" content="Platzi Conf Store" />
+        <meta name="twitter:description" content="Platzi Conf Store" />
+        <meta
+          name="twitter:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:title" content="Platzi Conf Store" />
+        <meta property="og:description" content="Platzi Conf Store" />
+        <meta
+          property="og:image"
+          content="https://s3.amazonaws.com/gndx.dev/gndxdev.png"
+        />
+        <meta property="og:url" content="platzistore.xyz" />
+        <meta property="og:site_name" content="Platzi Conf Store" />
+        <meta property="og:locale" content="es_ES" />
+        <meta property="og:type" content="article" />
+        <meta property="fb:app_id" content="ID_APP_FACEBOOK" />
+      </Head>
+      <Layout>
+        <Products products={ProductList.products} />
+      </Layout>
+    </>
   );
 }
